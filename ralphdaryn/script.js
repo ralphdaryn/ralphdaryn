82d25a57b2243a59fb57009135076cedcbcf05e9
ralphdaryn <74474293+ralphdaryn@users.noreply.github.com>
2023-02-01 20:42:33 -0500

    Add files via upload

---
+++ ralphdaryn/script.js
@@ -0,0 +1,53 @@
+// Mobile Navigation
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", function () {
+  headerEl.classList.toggle("nav-open");
+});
+
+// Smooth Scroll Animation
+const allLinks = document.querySelectorAll(".navlink-label");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("navlink-label"))
+      headerEl.classList.toggle("nav-open");
+  });
+});
+
+// Reveal Elements on Scroll
+window.addEventListener('scroll', reveal);
+
+function reveal(){
+  let reveals = document.querySelectorAll('.reveal');
+  for(let i = 0; i < reveals.length; i++){
+    let windowheight = window.innerHeight;
+    let revealTop = reveals[i].getBoundingClientRect().top;
+    let revealPoint = 150;
+
+    if(revealTop < windowheight - revealPoint){
+      reveals[i].classList.add('active');
+    }
+    else{
+      reveals[i].classList.remove('active');
+    }
+  }
+}
